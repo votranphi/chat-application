3fb0f5629ab3ea1ba3850081c9ecd18c3332c76e
taicyberr <114321172+terex2710@users.noreply.github.com>
2024-04-03 04:39:11 +0700

    Add project files.

---
+++ UserAccount/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace UserAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(134, 129);
+            this.tbName.Multiline = true;
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(201, 20);
+            this.tbName.TabIndex = 2;
+
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(134, 176);
+            this.tbPassword.Multiline = true;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(201, 20);
+            this.tbPassword.TabIndex = 3;
+
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DEMO LUU USER";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.password});
+            this.dataGridView1.Location = new System.Drawing.Point(440, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(312, 293);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.Width = 150;
+            // 
+            // password
+            // 
+            this.password.HeaderText = "Password";
+            this.password.Name = "password";
+            this.password.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(134, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn password;
+    }
+}
+
---
+++ UserAccount/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.Entity.Infrastructure;
+using System.Xml.Linq;
+
+namespace UserAccount
+{
+    public partial class Form1 : Form
+    {
+        string path = "data_table.db";
+        string cs = @"URI=file:" + Application.StartupPath + "\\data_table.db"; //database
+
+        SQLiteConnection con;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void data_Show()
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM test";
+            var cmd = new SQLiteCommand(stm, con);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Insert(0, dr.GetString(0), dr.GetString(1));
+
+            }
+        }
+
+        private void create_DB()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source=" + path))
+                {
+                    sqlite.Open();
+                    string sql = "create table test(name varchar(20), password varchar(12))";
+                    SQLiteCommand command = new SQLiteCommand(sql, sqlite);
+                    command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                //MessageBox.Show("Database can't create", "Warning");
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            create_DB();
+            data_Show();
+        }
+
+        //button insert data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SQLiteConnection(cs);
+            con.Open();
+            cmd = new SQLiteCommand(con);
+
+            try
+            {
+                cmd.CommandText = "INSERT INTO  test(name,password) VALUES(@name,@password)";
+
+                string NAME = tbName.Text;
+                string PASSWORD = tbPassword.Text;
+
+                cmd.Parameters.AddWithValue("@name", NAME);
+                cmd.Parameters.AddWithValue("@password", PASSWORD);
+
+                dataGridView1.ColumnCount = 2;
+                dataGridView1.Columns[0].Name = "Name";
+                dataGridView1.Columns[1].Name = "Password";
+
+                string[] row = new string[] { NAME, PASSWORD };
+                dataGridView1.Rows.Add(row);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't insert data", "Warning");
+                return;
+            }
+            con.Close();
+        }
+
+        //update button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(con);
+
+            try
+            {
+                cmd.CommandText = "UPDATE test Set password=@Password where name=@Name";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@Name", tbName.Text);
+                cmd.Parameters.AddWithValue("@Password", tbPassword.Text);
+
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_Show();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't update data", "Warning");
+                return;
+            }
+
+            con.Close();
+
+        }
+
+
+        //delete button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(con);
+
+            try
+            {
+                cmd.CommandText = "Delete FROM test where name=@Name";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@Name", tbName.Text);
+
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_Show();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't DELETE data", "Warning");
+                return;
+            }
+            con.Close();
+        }
+
+
+    }
+}
