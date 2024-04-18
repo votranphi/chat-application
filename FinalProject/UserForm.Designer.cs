118e7b080573c5cb1b109c12589c39020144132b
Vo Tran Phi <songokupkj@gmail.com>
2024-04-18 21:19:28 +0700

    feat: userform

---
+++ FinalProject/UserForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace FinalProject
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewItem listViewItem1 = new ListViewItem(new string[] { "User1" }, -1, Color.Empty, Color.Empty, new Font("Segoe UI", 14F));
+            ListViewItem listViewItem2 = new ListViewItem(new string[] { "User2" }, -1, Color.Empty, Color.Empty, new Font("Segoe UI", 14F));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserForm));
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            listView1 = new ListView();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(272, 70);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(700, 500);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(272, 576);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(650, 50);
+            textBox2.TabIndex = 0;
+            textBox2.Text = "Type something...";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F);
+            textBox3.Location = new Point(272, 14);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(650, 50);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "User ...\r\nStatus (connected, disconnected)\r\n";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = Properties.Resources.send_icon;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Location = new Point(922, 576);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 50);
+            button1.TabIndex = 3;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = Properties.Resources.settingIcon;
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Location = new Point(922, 14);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 50);
+            button2.TabIndex = 4;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            listView1.GridLines = true;
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1, listViewItem2 });
+            listView1.Location = new Point(10, 70);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(256, 556);
+            listView1.TabIndex = 5;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.List;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F);
+            textBox4.Location = new Point(10, 12);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(200, 50);
+            textBox4.TabIndex = 6;
+            textBox4.Text = "Find user...";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BackgroundImage = Properties.Resources.find_icon;
+            button3.BackgroundImageLayout = ImageLayout.Zoom;
+            button3.Location = new Point(216, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 50);
+            button3.TabIndex = 7;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.form_background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(984, 661);
+            Controls.Add(button3);
+            Controls.Add(textBox4);
+            Controls.Add(listView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Chat";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private ListView listView1;
+        private TextBox textBox4;
+        private Button button3;
+    }
+}
