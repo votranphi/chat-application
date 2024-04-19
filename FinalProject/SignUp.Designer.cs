ecf733b0b3a7545af2f558b228b998df9366181b
minhphuc2544 <22521116@ms.uit.edu.vn>
2024-04-19 09:49:04 +0700

    dieu chinh giao dien SignUp

--- FinalProject/SignUp.Designer.cs
+++ FinalProject/SignUp.Designer.cs
@@ -32,18 +32,21 @@
             textBox2 = new TextBox();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            pictureBox1 = new PictureBox();
             label4 = new Label();
             button2 = new Button();
             label6 = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            textBox5 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 14F);
-            textBox1.Location = new Point(404, 325);
+            textBox1.Location = new Point(425, 292);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(216, 32);
             textBox1.TabIndex = 0;
@@ -51,7 +54,7 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 14F);
-            textBox2.Location = new Point(404, 372);
+            textBox2.Location = new Point(425, 345);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(216, 32);
             textBox2.TabIndex = 1;
@@ -61,7 +64,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14F);
             label1.Image = Properties.Resources.form_background;
-            label1.Location = new Point(307, 328);
+            label1.Location = new Point(255, 295);
             label1.Name = "label1";
             label1.Size = new Size(97, 25);
             label1.TabIndex = 2;
@@ -72,33 +75,11 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14F);
             label2.Image = Properties.Resources.form_background;
-            label2.Location = new Point(307, 375);
+            label2.Location = new Point(255, 348);
             label2.Name = "label2";
             label2.Size = new Size(91, 25);
             label2.TabIndex = 3;
             label2.Text = "Password";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 16F);
-            label3.Image = Properties.Resources.form_background;
-            label3.Location = new Point(404, 230);
-            label3.Name = "label3";
-            label3.Size = new Size(216, 30);
-            label3.TabIndex = 4;
-            label3.Text = "Chat with your friend";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = Color.Transparent;
-            pictureBox1.BackgroundImage = Properties.Resources.chat3_png;
-            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox1.Location = new Point(452, 122);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(131, 105);
-            pictureBox1.TabIndex = 6;
-            pictureBox1.TabStop = false;
             // 
             // label4
             // 
@@ -111,7 +92,8 @@
             // button2
             // 
             button2.BackColor = SystemColors.Highlight;
-            button2.Location = new Point(452, 425);
+            button2.Font = new Font("Segoe UI", 14F);
+            button2.Location = new Point(461, 461);
             button2.Name = "button2";
             button2.Size = new Size(131, 40);
             button2.TabIndex = 8;
@@ -121,13 +103,70 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 16F);
+            label6.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label6.Image = Properties.Resources.form_background;
-            label6.Location = new Point(404, 282);
+            label6.Location = new Point(379, 114);
             label6.Name = "label6";
-            label6.Size = new Size(204, 30);
+            label6.Size = new Size(303, 45);
             label6.TabIndex = 10;
             label6.Text = "Create new account";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Image = Properties.Resources.form_background;
+            label3.Location = new Point(255, 242);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 25);
+            label3.TabIndex = 12;
+            label3.Text = "Port";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14F);
+            textBox3.Location = new Point(425, 239);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(216, 32);
+            textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.Image = Properties.Resources.form_background;
+            label5.Location = new Point(255, 190);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 25);
+            label5.TabIndex = 14;
+            label5.Text = "Server IP";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 14F);
+            textBox4.Location = new Point(425, 187);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(216, 32);
+            textBox4.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14F);
+            label7.Image = Properties.Resources.form_background;
+            label7.Location = new Point(255, 399);
+            label7.Name = "label7";
+            label7.Size = new Size(164, 25);
+            label7.TabIndex = 16;
+            label7.Text = "Confirm Password";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 14F);
+            textBox5.Location = new Point(425, 396);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(216, 32);
+            textBox5.TabIndex = 15;
             // 
             // SignUp
             // 
@@ -136,11 +175,15 @@
             BackgroundImage = Properties.Resources.form_background;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(984, 661);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
             Controls.Add(label6);
             Controls.Add(button2);
             Controls.Add(label4);
-            Controls.Add(pictureBox1);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox2);
@@ -149,7 +192,6 @@
             MinimumSize = new Size(1000, 700);
             Name = "SignUp";
             Text = "Chat";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -160,10 +202,14 @@
         private TextBox textBox2;
         private Label label1;
         private Label label2;
-        private Label label3;
-        private PictureBox pictureBox1;
         private Label label4;
         private Button button2;
         private Label label6;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label7;
+        private TextBox textBox5;
     }
 }
