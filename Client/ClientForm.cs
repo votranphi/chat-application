8ac1136eeedbb09d801252871e08fcc86a4dfbf0
Vo Tran Phi <songokupkj@gmail.com>
2024-04-21 00:06:15 +0700

    fix: disable close button in clientform

--- Client/ClientForm.cs
+++ Client/ClientForm.cs
@@ -332,6 +332,17 @@ namespace Client
             }));
         }
 
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
         #region UpdateThreadSafe
 
         private void UpdateOnlineUserDataGridViewThreadSafe(string text)
